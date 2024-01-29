cd6c341277624586fa49bb6c21ae9bb5a381f2b7
rpromani251 <rpromani251@gmail.com>
2024-01-29 13:10:29 -0500

    sphere completed

--- src/Shapes/Sphere.java
+++ src/Shapes/Sphere.java
@@ -12,7 +12,7 @@ public class Sphere extends Shape {
         final double RADIUS = FRAME_WIDTH * 0.125;
         final double THETA = 2*Math.PI/25;
 
-        points = new Point[51];
+        points = new Point[76];
 
         double deltaX = RADIUS / 5;
         double x = FRAME_WIDTH / 2.0 - deltaX / 2; // x0 = center of window
@@ -26,7 +26,7 @@ public class Sphere extends Shape {
             points[i+25] = new Point(x, y, 0);
         }
 
-        double z = FRAME_HEIGHT / 2.0 - RADIUS + deltaX; // y0 = center of window + height of radius
+        double z = -RADIUS + 10 * deltaX / 11; // y0 = center of window + height of radius
         y = FRAME_HEIGHT / 2.0; // y0 = center of window + height of radius
         x = FRAME_WIDTH / 2.0 - deltaX / 2; // x0 = center of window
 
@@ -38,15 +38,36 @@ public class Sphere extends Shape {
             points[i] = new Point(x, y, z);
         }
 
-        points[50] = new Point(FRAME_WIDTH / 2.0, FRAME_HEIGHT / 2.0, 0);
+        z = - deltaX / 2; // y0 = center of window + height of radius
+        y = FRAME_HEIGHT / 2.0 - 4 * RADIUS / 5; // y0 = center of window + height of radius
+        x = FRAME_WIDTH / 2.0; // x0 = center of window
 
-        lines = new Line[25];
+        for (int i = 0; i < 25; i++) {
 
-        for (int i = 0; i < lines.length - 1; i++) {
-            lines[i] = new Line(points[i], points[i+1]);
+            y += deltaX * Math.sin(i*THETA);
+            z += deltaX * Math.cos(i*THETA);
+
+            points[i+50] = new Point(x, y, z);
         }
 
+        points[75] = new Point(FRAME_WIDTH / 2.0, FRAME_HEIGHT / 2.0, 0);
+
+        lines = new Line[75];
+
+        for (int i = 0; i < 24; i++) {
+            lines[i] = new Line(points[i], points[i+1]);
+        }
         lines[24] = new Line(points[24], points[0]);
+
+        for (int i = 25; i < 49; i++ ) {
+            lines[i] = new Line(points[i], points[i+1]);
+        }
+        lines[49] = new Line(points[49], points[25]);
+
+        for (int i = 50; i < 74; i++ ) {
+            lines[i] = new Line(points[i], points[i+1]);
+        }
+        lines[74] = new Line(points[74], points[50]);
         
     }
 
